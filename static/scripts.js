d3bd8cf183f3bc801b50bf7f9977fb48586223d6
Tian Yu Zhang <tianyu23572@gmail.com>
2012-03-16 19:05:49 -0400

    changed url

---
+++ static/scripts.js
@@ -0,0 +1,34 @@
+function activate($elem) {
+    $elem.removeClass('clickable');
+    $elem.addClass('activated');
+    $elem.off('click', makeMove);
+}
+
+function makeMove(event) {
+    var $elem = $(event.target);
+    activate($elem);
+
+    var data = {
+        x: $elem.data('x'),
+        y: $elem.data('y'),
+        orientation: $elem.data('ori')
+    };
+    $.post('move/', data, showMove);
+}
+
+function showMove(jsonData) {
+    var data = $.parseJSON(jsonData);
+    var x = data['x'];
+    var y = data['y'];
+    var ori = data['orientation'];
+    var xSelector = '[data-x="' + x + '"]';
+    var ySelector = '[data-y="' + y + '"]';
+    var oriSelector = '[data-ori="' + ori + '"]';
+    $edge = $('.clickable').filter(oriSelector).filter(xSelector).filter(ySelector);
+    console.log($edge);
+    activate($edge);
+}
+
+$(function() {
+    $('.clickable').click(makeMove);
+});
